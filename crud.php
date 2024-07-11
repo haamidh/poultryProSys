adafafa11e6810db4bcbd10c086059e0ae4746d2
haamidh <haamidhansak@gmail.com>
2024-07-11 09:52:34 +0530

    Update crud.php
    change the order of inputs to functions.

--- crud.php
+++ crud.php
@@ -3,7 +3,7 @@ interface crud
 {
     public function create($user_id);
     public function read($user_id);
-    public function update($id,$user_id);
-    public function delete($id, $user_id);
+    public function update($user_id, $id);
+    public function delete($user_id, $id);
     public function readOne();
 }
