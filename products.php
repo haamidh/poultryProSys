7495ca8365850ba247d0f817c827d8f709e58586
Tharindu Devinda De Zoysa <tharindudevinda123456@gmail.com>
2024-08-04 20:52:07 +0530

    updated products.php
    created the backend functions for products page

--- products.php
+++ products.php
@@ -10,14 +10,85 @@ if (!isset($_SESSION['user_id'])) {
     header("Location: login.php?msg=Please Login before Proceeding");
     exit();
 }
-$user_id = $_SESSION["user_id"];
+
+$farm_id = $_SESSION["user_id"];
 $database = new Database();
 $con = $database->getConnection();
-$farm = CheckLogin::checkLoginAndRole($user_id, 'farm');
+$farm = CheckLogin::checkLoginAndRole($farm_id, 'farm');
 $frame = new Frame();
 $frame->first_part($farm);
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $product_id = getLastProductId($con, $farm_id);
+    $product_name = $_POST['product_name'];
+    $quantity = $_POST['quantity'];
+    $unit = $_POST['unit']; 
+    $category_id = $_POST['category_id'];
+    $product_price = $_POST['selling_price'];
+    $product_img = $_FILES['product_img'];
+    $description = $_POST['product_description'];
 
+    // Handle file upload
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($product_img["name"]);
+    move_uploaded_file($product_img["tmp_name"], $target_file);
+
+    if ($con == false) {
+        die("Error Establishing Connection: " . $con->errorInfo());
+    }
+
+    if (addNewProduct($con, $product_id, $farm_id, $product_name, $quantity, $unit, $category_id, $product_price, $target_file, $description)) {
+        header('Location: products.php?msg=Data Updated Successfully&user_id=' . $farm_id);
+        ob_end_flush(); // Flush the buffer
+        exit();
+    } else {
+        echo "Record not added";
+    }
+}
+
+function getLastProductId($con, $farm_id){
+    $query = $con->prepare("SELECT product_id FROM products WHERE farm_id=? ORDER BY product_id DESC LIMIT 1");
+
+    if (!$query) {
+        die("Running Query failed: " . $con->errorInfo()[2]);
+    }
+
+    $query->bindParam(1, $farm_id, PDO::PARAM_INT);
+    $query->execute();
+    $row = $query->fetch(PDO::FETCH_ASSOC);
+
+    if (!$row) {
+        return 'P0001';
+    } else {
+        $lastId = $row['product_id'];
+        $numSuffix = intval(substr($lastId, 1));
+        $updatedId = sprintf('%04d', $numSuffix + 1);
+        return 'P' . $updatedId;
+    }
+}
+
+function addNewProduct($con, $product_id, $farm_id, $product_name, $quantity, $unit, $category_id, $product_price, $product_img, $description)
+{
+    $query = $con->prepare('INSERT INTO products (product_id, farm_id, product_name, quantity, unit, category_id, product_price, product_img, description) VALUES (:product_id, :user_id, :product_name, :quantity, :unit, :category_id, :product_price, :product_img, :description)');
+
+    $query->bindParam(':product_id', $product_id);
+    $query->bindParam(':user_id', $farm_id);
+    $query->bindParam(':product_name', $product_name);
+    $query->bindParam(':quantity', $quantity);
+    $query->bindParam(':unit', $unit);
+    $query->bindParam(':category_id', $category_id);
+    $query->bindParam(':product_price', $product_price);
+    $query->bindParam(':product_img', $product_img);
+    $query->bindParam(':description', $description);
+
+    return $query->execute();
+}
+
+function fetchCategories($con) {
+    $query = $con->prepare('SELECT * FROM product_categories');
+    $query->execute();
+    return $query->fetchAll(PDO::FETCH_ASSOC);
+}
 ?>
 
 <div class="container contentArea" style="margin-left: -30px;margin-right:10px">
@@ -28,20 +99,22 @@ $frame->first_part($farm);
             <div class="card-header card text-white" style="background-color: #40826D;">
                 <h2 class="display-6 text-center" style="font-size: 30px; font-weight:500;">Product Details</h2>
             </div>
-            <form class="row g-3" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+            <form class="row g-3" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" enctype="multipart/form-data">
 
-                <input type="hidden" class="form-control" value="<?php echo $user_id; ?>" name="user_id" id="user_id" readonly>
+                <input type="hidden" class="form-control" value="<?php echo $farm_id; ?>" name="user_id" id="user_id" readonly>
                 <div class="col-md-12">
                     <label for="product_id" class="form-label">Product ID:</label>
-                    <input class="form-control" type="text" name="product_id" id="_id" readonly>
+                    <input class="form-control" type="text" name="product_id" id="product_id" value="<?php echo getLastProductId($con, $farm_id); ?>" readonly>
                 </div>
                 <div class="col-md-12">
                     <label for="product_category" class="form-label">Select Category:</label>
-                    <select class="form-select">
-                        <option disabled>Select Category</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
+                    
+                    <select class="form-select" name="category_id">
+                    <?php  
+                    $categories = fetchCategories($con); 
+                    foreach($categories as $category) {?>
+                        <option value="<?php echo $category["category_id"] ?>"><?php echo $category["category_name"]; ?></option> 
+                    <?php } ?>
                     </select>
                 </div>
 
@@ -50,10 +123,28 @@ $frame->first_part($farm);
                     <input class="form-control" type="text" name="product_name" id="product_name" required>
                 </div>
 
+                <div class="col-md-12">
+                    <label for="quantity" class="form-label">Quantity:</label>
+                    <input class="form-control" type="number" name="quantity" id="quantity" required>
+                </div>
+
+                <div class="row mt-3">
+                    <div class="col-md-4">
+                        <label for="quantity" class="form-label">Unit:</label>
+                    </div>
+                    <div class="col-md-4 form-check">
+                        <input type="radio" class="form-check-input" name="unit" value="kilogram" id="kilogram">
+                        <label class="form-check-label" for="kilogram">kg</label>
+                    </div>
+                    <div class="col-md-4 form-check">
+                        <input type="radio" class="form-check-input" name="unit" value="pieces" id="pieces">
+                        <label class="form-check-label" for="pieces">Pieces</label>
+                    </div>
+                </div>
 
                 <div class="col-md-12">
                     <label for="selling_price" class="form-label">Selling Price:</label>
-                    <input class="form-control" type="text" name="selling_price" id="selling_price" required>
+                    <input class="form-control" type="number" name="selling_price" id="selling_price" required>
                 </div>
 
                 <div class="col-md-12">
@@ -67,7 +158,7 @@ $frame->first_part($farm);
                 </div>
 
                 <div class="col-md-12" style="text-align: center;">
-                    <button type="submit" class="btn btn-primary" name="add_medicine">Add Product</button>
+                    <button type="submit" class="btn btn-primary" name="add_product">Add Product</button>
                 </div>
             </form>
         </div>
