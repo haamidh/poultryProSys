26be602fa99d1a11c7738e027d92a338f4b411ff
haamidh <haamidhansak@gmail.com>
2024-09-29 19:54:52 +0530

    checkout page
    updated checkout page

---
+++ UpdatedSystem/checkout.php
@@ -0,0 +1,46 @@
+<?php
+$merchant_id = 1227852;
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $order_id = $_POST['order_id'];
+    $hash = strtoupper(
+        md5(
+            $merchant_id . 
+            $order_id . 
+            number_format($amount, 2, '.', '') . 
+            $currency .  
+            strtoupper(md5($merchant_secret)) 
+        ) 
+    );
+}
+
+
+
+
+?>
+<html>
+<body>
+<form method="post" action="https://sandbox.payhere.lk/pay/checkout">   
+    <input type="hidden" name="merchant_id" value="1227852">    <!-- Replace your Merchant ID -->
+    <input type="hidden" name="return_url" value="http://sample.com/return">
+    <input type="hidden" name="cancel_url" value="http://sample.com/cancel">
+    <input type="hidden" name="notify_url" value="http://sample.com/notify">  
+    </br></br>Item Details</br>
+    <input type="text" name="order_id" value="ItemNo12345">
+    <input type="text" name="items" value="Door bell wireless">
+    <input type="text" name="currency" value="LKR">
+    <input type="text" name="amount" value="1000">  
+    </br></br>Customer Details</br>
+    <input type="text" name="first_name" value="Saman">
+    <input type="text" name="last_name" value="Perera">
+    <input type="text" name="email" value="samanp@gmail.com">
+    <input type="text" name="phone" value="0771234567">
+    <input type="text" name="address" value="No.1, Galle Road">
+    <input type="text" name="city" value="Colombo">
+    <input type="hidden" name="country" value="Sri Lanka">
+    <?php  ?>
+    <input type="hidden" name="hash" value="098F6BCD4621D373CADE4E832627B4F6">    <!-- Replace with generated hash -->
+    <input type="submit" value="Buy Now">   
+</form> 
+</body>
+</html>
