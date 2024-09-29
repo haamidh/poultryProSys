87be8b8b0caf8e80b5aea4b1eecbbca61d61c17d
haamidh <haamidhansak@gmail.com>
2024-09-29 19:05:58 +0530

    updated marketplace
    market place minor errors resolved

--- UpdatedSystem/processOrder.php
+++ UpdatedSystem/processOrder.php
@@ -1,7 +1,8 @@
 <?php
-    require_once 'marketplaceFrame.php';
-    require '../classes/config.php';
-    require 'marketPlaceCRUD.php';
+    require_once 'marketplace/marketplaceFrame.php';
+    require 'classes/config.php';
+    require 'marketplace/marketPlaceCRUD.php';
+    
     
     $marketPlaceFrame = new marketPlaceFrame();
     $marketPlaceFrame->navbar();
@@ -19,7 +20,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
-  <link rel="stylesheet" href="marketPlaceStyle.css">
+  <link rel="stylesheet" href="marketplace/marketPlaceStyle.css">
   <title>MarketPlace - PoultryPro</title>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
